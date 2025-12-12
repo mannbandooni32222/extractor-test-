146de90b748c15ffbbdaf45fe69b39e0780bc016
mannbandooni32222 <116864689+mannbandooni32222@users.noreply.github.com>
2025-12-12 05:52:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-
+import json
+from pathlib import Path
+import hashlib
+import streamlit as st
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup
@@ -85,4 +88,48 @@ hide_streamlit_style = """
     </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+
+
+USER_DB = Path("users.json")
+
+def load_users():
+    if USER_DB.exists():
+        with open(USER_DB, "r") as f:
+            return json.load(f)
+    return {"users": []}
+
+def save_users(data):
+    with open(USER_DB, "w") as f:
+        json.dump(data, f, indent=4)
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def authenticate_user(email, password):
+    users = load_users()["users"]
+    hashed = hash_password(password)
+    
+    for user in users:
+        if user["email"] == email and user["password"] == hashed:
+            return True
+    return False
+
+def register_user(email, password):
+    users_data = load_users()
+    
+    # Check if email exists
+    for u in users_data["users"]:
+        if u["email"] == email:
+            return False  # email exists
+
+    users_data["users"].append({
+        "email": email,
+        "password": hash_password(password),
+        "plan": "free"   # free plan by default
+    })
+
+    save_users(users_data)
+    return True
 
